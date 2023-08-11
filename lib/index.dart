9bf12f26af88af1854e761757fc87a34a37e24a0
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-08-11 11:35:49 +0000

    test audio recording =

---
+++ lib/index.dart
@@ -0,0 +1,2 @@
+// Export pages
+export '/pages/speaking/speaking_widget.dart' show SpeakingWidget;
---
+++ lib/pages/speaking/speaking_model.dart
@@ -0,0 +1,36 @@
+import '/flutter_flow/flutter_flow_audio_player.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/permissions_util.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:provider/provider.dart';
+import 'package:record/record.dart';
+
+class SpeakingModel extends FlutterFlowModel {
+  ///  Local state fields for this page.
+
+  bool isRecord = false;
+
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  String? output;
+  Record? audioRecorder;
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {
+    unfocusNode.dispose();
+  }
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
